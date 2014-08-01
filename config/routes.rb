068938a1e453830a96af80e7d9b50bbfd8e6e9bf
Michael Pope <amorphid@captaincoder.io>
2014-08-01 14:18:48 -0700

    Add route for /jobs/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :jobs, only: [:index]
+  resources :jobs, only: [:index, :new]
 end
--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -4,4 +4,8 @@ describe JobsController do
   it "routes to #index" do
     get("/jobs").should route_to("jobs#index")
   end
+
+  it "routes to #new" do
+    get("/jobs/new").should route_to("jobs#new")
+  end
 end
